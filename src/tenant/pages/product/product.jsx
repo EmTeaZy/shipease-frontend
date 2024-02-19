7b80c9a6acb83e0c41cb342348ac01f8c4947f7d
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-02-19 17:03:54 +0500

    Modity product page | Use grid and styling the product Component|

--- src/tenant/pages/product/product.jsx
+++ src/tenant/pages/product/product.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-import theme from "../../../theme";
-import { Grid, Box } from '@mui/material';
 
-const Product = ({ product, onAddToCart }) => {
-  const { id, name, image, price, description } = product;
-
+const Product = ({ product, onAddToCart, productStyles, productImageStyles, productImageContainerStyles, producth3, productp }) => {
   return (
-    <div className="product">
-    <Grid item md={3} xs={12}>
-          <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          width: '56%',
-          marginLeft: '5%',
-          height: '100%',
-          marginTop: '35px',
-          backgroundColor: theme.palette.primary.backgroundColor
-        }}
-      >
-        <img src={image} alt={name} className="product-image" style={{ marginTop: '10px', marginBottom: '20px',marginLeft: '10px',}}/>
-        <h3 className="product-name" style={{ marginTop: '-20px'}}>{name}</h3>
-        <p className="product-price" style={{ marginTop: '-20px', marginLeft:'-130px'}}>${price}</p>
-      </Box>
-      </Grid>
+    <div className="product" style={productStyles}>
+      <div style={productImageContainerStyles}>
+        <img src={product.image} alt={product.name} style={productImageStyles} />
+      </div>
+      <h3 style={producth3}>{product.name}</h3>
+      <p style={productp} >${product.price}</p>
     </div>
   );
 };
